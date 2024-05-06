88e06191fc8611a19edc45aa9247de6c3c8b93ba
Ayush yadav <ayushyadav9990@gmail.com>
2024-05-06 23:46:33 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import SignaturePad from "./Components/Signature/Signature";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Components/LandingPage/Landing";
 import Navbarr from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Router>
+      <HashRouter>
         <Navbarr></Navbarr>
         <Routes>
           <Route path="/" element={<Landing />}></Route>
           <Route path="/signature" element={<SignaturePad />}></Route>
         </Routes>
-      </Router>
+      </HashRouter>
     </>
   );
 }
